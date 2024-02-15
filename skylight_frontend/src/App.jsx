87ecdb5281f9f5d6d349be6bfb48fe41d1e73d6a
Shreyas <shreyas2003w@gmail.com>
2024-02-15 20:17:00 +0530

    Added NavBar, Footer, and css from chakra UI

--- skylight_frontend/src/App.jsx
+++ skylight_frontend/src/App.jsx
@@ -1,16 +1,59 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import { Routes, Route, useNavigate} from 'react-router-dom'
-import Home from './container/Home'
-import Login from './components/Login'
+/* eslint-disable react/prop-types */
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css'; 
+import { ChakraProvider } from '@chakra-ui/react';
+import Layout from './components/Layout';
+import Home from './container/Home';
+import Login from './components/Login';
 
-const App = () => {
+const App = ({ Component, pageProps }) => {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    NProgress.configure({ showSpinner: false });
+
+    const routeChangeStart = () => {
+      NProgress.start();
+    };
+
+    const routeChangeComplete = () => {
+      NProgress.done();
+    };
+
+    window.addEventListener('routeChangeStart', routeChangeStart);
+    window.addEventListener('routeChangeComplete', routeChangeComplete);
+
+    return () => {
+      window.removeEventListener('routeChangeStart', routeChangeStart);
+      window.removeEventListener('routeChangeComplete', routeChangeComplete);
+    };
+  }, []);
+
+  const isPath = location.pathname === '/login';
+
   return (
-    <Routes>
-      <Route path='login' element={<Login />} />
-      <Route path='*' element={<Home />} />
-    </Routes>
-  )
-}
+    <>
+      <ChakraProvider>
+        {!isPath && (
+          <Layout>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </Layout>
+        )}
+        </ChakraProvider>
+        {isPath && (
+          <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        )}
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ skylight_frontend/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { Menu, MenuButton, MenuList, MenuItem, IconButton, Flex, Box, Spacer } from '@chakra-ui/react';
+import { FcMenu, FcHome, FcAbout } from 'react-icons/fc';
+import { BsSearch } from 'react-icons/bs';
+import { FiKey } from 'react-icons/fi';
+
+const Navbar = () => (
+  <Flex p='2' borderBottom='1px' borderColor='gray.100'>
+    <Box fontSize='3xl' color='blue.400' fontWeight='bold'>
+      <a href='/' style={{ paddingLeft: '2' }}>Skylight</a>
+    </Box>
+    <Spacer />
+    <Box>
+      <Menu>
+        <MenuButton as={IconButton} icon={<FcMenu />} variant='outline' color='red.400' />
+        <MenuList>
+          <MenuItem>
+            <a href='/'>
+              <FcHome /> Home
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a href='/search'>
+              <BsSearch /> Search
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a href='/search?purpose=for-sale'>
+              <FcAbout /> Buy Property
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a href='/search?purpose=for-rent'>
+              <FiKey /> Rent Property
+            </a>
+          </MenuItem>
+        </MenuList>
+      </Menu>
+    </Box>
+  </Flex>
+);
+
+export default Navbar;
